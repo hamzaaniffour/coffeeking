b8b37fa331419f6dc2a01b6cfe784f6f31cd46a6
Hamza Aniffour <aniffour.dev@gmail.com>
2024-05-10 07:11:16 +0100

    Added Aria Label

--- components/Headers/Logo.tsx
+++ components/Headers/Logo.tsx
@@ -6,14 +6,13 @@ import { GiCoffeeBeans } from "react-icons/gi";
 const Logo = () => {
   return (
     <div>
-      <Link href="/" className="text-white">
-        <GiCoffeeBeans className="h-6 w-6 text-amber-400 float-left -mt-[2px] mr-1" /> <Image
+      <Link href="/" className="text-amber-400">
+        <Image
           src="https://svgur.com/i/15m5.svg"
           alt="logo"
           priority={true}
           width={140}
           height={50}
-          className="mt-0.5"
         />
       </Link>
     </div>
