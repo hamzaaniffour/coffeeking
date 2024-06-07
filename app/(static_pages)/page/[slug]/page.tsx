8f420b3721463428e0b7c8451d87e75b8868aa15
Hamza Aniffour <aniffour.dev@gmail.com>
2024-06-07 02:46:02 +0100

    Restore All Setting

--- app/(static_pages)/page/[slug]/page.tsx
+++ app/(static_pages)/page/[slug]/page.tsx
@@ -1,8 +1,83 @@
+import HomeSidebar from "@/components/Sidebars/HomeSidebar";
+import Link from "next/link";
 import React from "react";
-import SinglePage from "@/components/Dynamics/SinglePage";
 
-const Page = ({ params }: { params: { slug: string } }) => {
-  return <SinglePage params={params} />;
+const getSinglePost = async (postSlug: string) => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URLS}/wp-json/wp/v2/pages?slug=${postSlug}&_embed`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      cache: 'no-store',
+    }
+  );
+  const data = await response.json();
+  return data;
 };
 
-export default Page;
+function stripHtml(html: string) {
+  return html.replace(/<[^>]*>?/gm, "");
+}
+
+export async function generateStaticParams() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URLS}/wp-json/wp/v2/pages`);
+  const pages = await response.json();
+
+  return pages.map((page: any) => ({
+    slug: page.slug,
+  }));
+}
+
+export async function generateMetadata({ params }: { params: { slug: string } }) {
+  const posts = await getSinglePost(params.slug);
+  if (posts.length === 0) {
+    throw new Error("No post found for the given slug.");
+  }
+  const post = posts[0];
+  return {
+    title: post.title.rendered,
+    description: stripHtml(post.excerpt.rendered),
+    robots: "follow, index",
+  };
+}
+
+const SinglePage = async ({ params }: { params: { slug: string } }) => {
+  const data = await getSinglePost(params.slug);
+
+  return (
+    <div className="mt-20">
+      <div className="lg:flex gap-10">
+        <div className="lg:w-9/12">
+          {data.map((page: any) => (
+            <div key={page.slug}>
+              <h1 className="text-4xl md:text-5xl lg:text-4xl font-bold xl:text-4xl uppercase mb-4 text-black">
+                {page.title.rendered}
+              </h1>
+              <div className="text-sm breadcrumbs mb-8">
+                <ul>
+                  <li>
+                    <Link href="/">Home</Link>
+                  </li>
+                  <li>Page</li>
+                  <li className="font-semibold">{page.title.rendered}</li>
+                </ul>
+              </div>
+              <div
+                className="single-content text-slate-800 font-light text-lg"
+                dangerouslySetInnerHTML={{ __html: page.content.rendered }}
+              ></div>
+            </div>
+          ))}
+        </div>
+        <div className="border-0 lg:border-r-[1px] lg:border-gray-100 xl:border-r-[2px] xl:border-gray-100"></div>
+        <div className="lg:w-3/12 hidden md:hidden lg:block xl:block">
+          <HomeSidebar />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SinglePage;
